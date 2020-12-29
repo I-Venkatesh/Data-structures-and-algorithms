ef63ae1af84971216546ce80d871ddc41cb15536
I-Venkatesh <venkatesh.i18@iiits.in>
2020-12-29 12:59:52 +0530

    Longest prefix suffix added.

---
+++ String/longestPrefixSuffix.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+#include <bits/stdc++.h>
+int lps(string s)
+{
+    int n = s.length();
+    int lps[n];
+    lps[0] = 0;
+    int len = 0;
+    int i = 1;
+    while (i < n)
+    {
+        if (s[i] == s[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len != 0)
+            {
+                len = lps[len - 1];
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps[n - 1];
+}
+int main()
+{
+    string s;
+    cin >> s;
+    cout << lps(s) << "\n";
+}
