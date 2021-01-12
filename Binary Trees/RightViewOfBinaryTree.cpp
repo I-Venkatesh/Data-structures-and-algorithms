e2a59331c18ff28c19220d9e2dcd26c47c657aa5
I-Venkatesh <venkatesh.i18@iiits.in>
2021-01-12 15:12:57 +0530

    Right view of binary tree added.

---
+++ Binary Trees/RightViewOfBinaryTree.cpp
@@ -0,0 +1,82 @@
+using namespace std;
+#include<bits/stdc++.h>
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+Node* createNode(int val)
+{
+    struct Node* temp=new Node;
+    temp->data=val;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+Node* buildTree(vector<int> n)
+{
+    if(n.size()==0||n[0]==-1)
+    {
+        return NULL;
+    }
+    queue<Node*> q;
+    Node* root=createNode(n[0]);
+    q.push(root);
+    int i=1;
+    while(!q.empty()&&i<n.size())
+    {
+        Node* currNode=q.front();
+        q.pop();
+        if(n[i]!=-1)
+        {
+            currNode->left=createNode(n[i]);
+            q.push(currNode->left);
+            i++;
+        }
+        if(i>=n.size())
+        {
+            break;
+        }
+        Node* curr=createNode(n[i]);
+        if(n[i]!=-1)
+        {
+            currNode->right=curr;
+            q.push(currNode->right);
+            i++;
+        }
+    }
+    return root;
+}
+void dfs(Node* root,int level,int* maxLevel)
+{
+    if(root==0)
+    {
+        return;
+    }
+    if(level>*maxLevel)
+    {
+        cout<<root->data<<" ";
+        *maxLevel=level;
+    }
+    dfs(root->right,level+1,maxLevel);
+    dfs(root->left,level+1,maxLevel);
+}
+void RightView(Node* root)
+{
+    int maxLevel=0;
+    dfs(root,1,&maxLevel);
+}
+int main()
+{
+    int nodes;
+    cin>>nodes;
+    vector<int> n;
+    for(int i=0;i<nodes;i++)
+    {
+        int x;
+        cin>>x;
+        n.push_back(x);
+    }
+    Node* root=buildTree(n);
+    RightView(root);
+}
