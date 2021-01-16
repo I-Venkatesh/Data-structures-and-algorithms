dd2b252b92a611886eb89f61856b92a520ff1b20
I-Venkatesh <venkatesh.i18@iiits.in>
2021-01-16 13:22:03 +0530

    DFS of graph added.

---
+++ Graph/DFS.cpp
@@ -0,0 +1,40 @@
+using namespace std;
+#include <bits/stdc++.h>
+int main()
+{
+    int v, e;
+    cin >> v >> e;
+    vector<int> graph[100005];
+    for (int i = 0; i < e; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        graph[x].push_back(y);
+    }
+    vector<int> ans;
+    int visited[100005];
+    memset(visited, 0, sizeof(visited));
+    stack<int> q;
+    q.push(2);
+    while (!q.empty())
+    {
+        int x = q.top();
+        if (!visited[x])
+        {
+            ans.push_back(x);
+            visited[x] = 1;
+        }
+        q.pop();
+        for (int i = 0; i < graph[x].size(); i++)
+        {
+            if (!visited[graph[x][i]])
+            {
+                q.push(graph[x][i]);
+            }
+        }
+    }
+    for(auto i:ans)
+    {
+        cout<<i<<" ";
+    }
+}
