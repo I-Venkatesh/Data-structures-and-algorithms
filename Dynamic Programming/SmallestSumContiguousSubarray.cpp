598be165724a5b2d12533ca3608531a359f841db
I-Venkatesh <venkatesh.i18@iiits.in>
2021-01-24 15:36:12 +0530

    Smallest sum contigugous subarray added.

---
+++ Dynamic Programming/SmallestSumContiguousSubarray.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+#include<bits/stdc++.h>
+int maxSubarraySum(int arr[], int n){
+    
+    // Your code here
+    int dp[n+1];
+    dp[0]=arr[0];
+    int sum=dp[0];
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]<dp[i-1]+arr[i])
+        {
+            dp[i]=arr[i];
+            sum=arr[i];
+        }
+        else{
+            dp[i]=dp[i-1]+arr[i];
+            sum+=arr[i];
+        }
+    }
+    int maxi=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        maxi=min(maxi,dp[i]);
+    }
+    return maxi;
+}
+int main()
+{
+    int num;
+    cin>>num;
+    int arr[num];
+    for(int i=0;i<num;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<maxSubarraySum(arr,num)<<"\n";
+}
