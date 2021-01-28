656da39abe17376ecfc77cec3ec84c48b287ee22
I-Venkatesh <venkatesh.i18@iiits.in>
2021-01-28 13:33:10 +0530

    Queue using two stacks added

---
+++ Stacks and Queues/QueueUsingTwoStacks.cpp
@@ -0,0 +1,29 @@
+void QueueStack :: push(int x)
+{
+    q1.push(x);
+}
+
+int QueueStack :: pop()
+{
+        int ans=-1;
+        if(q1.size()>0)
+        {
+            while(q1.size()>1)
+            {
+                // cout<<q1.front()<<"\n";
+                q2.push(q1.front());
+                q1.pop();
+            }
+            ans=q1.front();
+        }
+        if(q1.size()>0)
+        {
+            q1.pop();
+        }
+        while(!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return ans;
+}
