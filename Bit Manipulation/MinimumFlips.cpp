04956a377ee27f63b0254b38ae2a76563ed88ebd
I-Venkatesh <venkatesh.i18@iiits.in>
2021-03-15 15:21:54 +0530

    Minimum Flips to Make a OR b Equal to c added

---
+++ Bit Manipulation/MinimumFlips.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int count=0;
+       for(int i=0;i<32;i++)
+       {
+           if((c&(1<<i))==0)
+           {
+              if((a&(1<<i))!=0)
+              {
+                  count++;
+              }
+              if((b&(1<<i))!=0)
+              {
+                  count++;
+              }
+           }
+           else{
+               if((a&(1<<i))||(b&(1<<i)))
+               {
+                   continue;
+               }
+               else{
+                   count++;
+                }
+            }
+       }
+        return count;
+    }
+};
