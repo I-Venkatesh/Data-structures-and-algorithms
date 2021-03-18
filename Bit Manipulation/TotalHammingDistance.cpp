730fc8669d4e0a58335f4dd58634e3e732b9f251
I-Venkatesh <venkatesh.i18@iiits.in>
2021-03-18 11:22:22 +0530

    Total hamming distance added

---
+++ Bit Manipulation/TotalHammingDistance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            int one=0,zero=0;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(nums[j]&(1<<i))
+                {
+                    one++;
+                }
+                else{
+                    zero++;
+                }
+            }
+            ans+=(one*zero);
+        }
+        return ans;
+    }
+};
