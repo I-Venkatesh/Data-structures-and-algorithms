61cac27aa7959418ad31ccdc1ec8d378718aaf18
I-Venkatesh <venkatesh.i18@iiits.in>
2021-07-30 23:27:57 +0530

    Subarray with given sum added

---
+++ Miscellaneous/SubarrayGivenSum.cpp
@@ -0,0 +1,52 @@
+vector<int> subarraySum(int arr[], int n, int s)
+{
+    int i = 0, j = 0;
+    int sum = 0;
+    vector<int> ans;
+
+    while (j < n && i <= j)
+    {
+        if (sum == s)
+        {
+            ans.push_back(i + 1);
+            ans.push_back(j);
+            return ans;
+        }
+        else
+        {
+            if (sum > s)
+            {
+                sum -= arr[i];
+                i++;
+            }
+            else
+            {
+                sum += arr[j];
+                // cout<<sum<<"\n";
+                j++;
+            }
+        }
+    }
+    while (i < n && sum > n)
+    {
+        if (sum == s)
+        {
+            ans.push_back(i + 1);
+            ans.push_back(j);
+            return ans;
+        }
+        sum -= arr[i];
+        i++;
+    }
+    for (int x = 0; x < n; x++)
+    {
+        if (arr[x] == s)
+        {
+            ans.push_back(x + 1);
+            ans.push_back(x + 1);
+            return ans;
+        }
+    }
+    ans.push_back(-1);
+    return ans;
+}
