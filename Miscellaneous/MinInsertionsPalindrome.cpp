5ab25db0cb395dec82ed711ff2317d0097be2c32
I-Venkatesh <venkatesh.i18@iiits.in>
2021-08-13 18:56:33 +0530

    Min insertion palindrome added

---
+++ Miscellaneous/MinInsertionsPalindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[501][501];
+    int helper(string &s,int i,int j)
+    {
+        if(i>=j)
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        if(s[i]==s[j])
+        {
+            return dp[i][j]=helper(s,i+1,j-1);
+        }
+        return dp[i][j]=1+min(helper(s,i+1,j),helper(s,i,j-1));
+    }
+    int minInsertions(string s) {
+        memset(dp,-1,sizeof(dp));
+        return helper(s,0,s.length()-1);
+    }
+};
