33980c872c4e28002a8717cfdc2032d4981d0932
I-Venkatesh <venkatesh.i18@iiits.in>
2021-08-15 14:04:18 +0530

    Stone game IV added

---
+++ Miscellaneous/StoneGameIV.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[100005];
+    int solve(int n)
+    {
+        if(n<=0)
+        {
+            return false;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        bool ans=false;
+        for(int i=1;i*i<=n;i++)
+        {
+            ans=ans||!solve(n-i*i);
+        }
+        return dp[n]=ans;
+    }
+    bool winnerSquareGame(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
